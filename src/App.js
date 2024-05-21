a97fbaff976a68074c454d251600b22dbbb87d2f
Rahul <gaherwarrahul133@gmail.com>
2024-05-21 12:22:17 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import { Header,Footer } from './components';
+import { Allroute } from './routes/Allroute';
+
+function App() {
+  return (
+   <>
+   <Header/>
+   <Allroute/>
+   <Footer/>
+   </>
+  );
+}
+
+export default App;
